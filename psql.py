1f084d6fdc8fbe7f6f6aa61a4b24a7855df9ba3b
harsh_rana_dev <hr2322808@gmail.com>
2026-04-02 14:35:26 +0530

    fix

--- psql.py
+++ psql.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine, MetaData, String, Integer, Column, Table
 
-# Create engine
 engine = create_engine("postgresql://user:password@localhost/db", echo=True)
 
-# Metadata
 meta = MetaData()
 
-# Define table
 bots = Table(
     "bots",
     meta,
@@ -15,12 +12,9 @@ bots = Table(
     Column('age', Integer, nullable=False)
 )
 
-# Create table
 meta.create_all(engine)
 
-# Insert data using transaction
-with engine.begin() as conn:
-    insert_stmt = bots.insert().values(name='mik', age=45)
-    conn.execute(insert_stmt)
 
-    
+with engine.begin() as conn:
+    insert_stmt = bots.insert().values(name='milk', age=1)
+    conn.execute(insert_stmt)

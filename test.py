9e23f10a42388789f24f8ec122f66bacc9280720
harsh_rana_dev <hr2322808@gmail.com>
2026-04-02 19:03:08 +0530

    pytest revesion

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
 import pytest
-from day_3 import clean_data
-
-def test_data():
-    data = [
-        {"ticker": "AAPL", "price": 100},
-        {"ticker": "AAPL", "price": -50},
-        {"ticker": "MSFT", "price": 200},
-        {"ticker": "MSFT", "price": None},
-        {"ticker": None, "price": 300},
-    ]
-
-    result = clean_data(data)
-
-    expected = [
-        {"ticker": "AAPL", "price": 100},
-        {"ticker": "MSFT", "price": 200},
-    ]
-
-    assert result == expected
+from day_67 import

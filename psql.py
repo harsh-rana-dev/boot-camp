c3cc3cda3b9633f9e85549258320ce592cfa157a
harsh_rana_dev <hr2322808@gmail.com>
2026-04-02 15:25:59 +0530

    fahhhhh

--- psql.py
+++ psql.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, String, Integer, Column, Table
+from sqlalchemy import create_engine, MetaData, String, Integer, Column, Table, Float, ForeignKey
 
 engine = create_engine("postgresql://user:password@localhost/db", echo=True)
 
@@ -12,13 +12,31 @@ bots = Table(
     Column('age', Integer, nullable=False)
 )
 
+things = Table(
+    "things",
+    meta,
+    Column('id', Integer, primary_key=True),
+    Column("des", String, nullable=False),
+    Column("value", Float),
+    Column("owner", Integer, ForeignKey("bots.id"))
+)
+
 meta.create_all(engine)
 
 with engine.begin() as conn:
-    update_stmt = (
-        bots.update()
-        .where(bots.c.name == 'milk')
-        .values(age=50)
-    )
 
-    result = conn.execute(update_stmt)
+    conn.execute(bots.insert(), [
+        {'name': 'a', 'age': 10},
+        {'name': 'b', 'age': 20},
+        {'name': 'c', 'age': 30},
+        {'name': 'd', 'age': 40},
+        {'name': 'e', 'age': 50}
+    ])
+
+    conn.execute(things.insert(), [
+        {'owner': 1, 'des': 'something i dont know', 'value': 456},
+        {'owner': 2, 'des': 'something i dont know', 'value': 556},
+        {'owner': 3, 'des': 'something i dont know', 'value': 656},
+        {'owner': 4, 'des': 'something i dont know', 'value': 47556},
+        {'owner': 5, 'des': 'something i dont know', 'value': 456874}
+    ])

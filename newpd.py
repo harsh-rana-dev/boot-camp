9e38f500d3272fe0f3714d65ec72c7534e368cbf
harsh_rana_dev <hr2322808@gmail.com>
2026-04-18 20:20:32 +0530

    hehehe

--- newpd.py
+++ newpd.py
@@ -1,7 +1,23 @@
 import pandas as pd 
 
-data = [12.25, 12.25, 12.25]
+exp = {
+    'pizza': [12, 34, 41, 75],
+    'age': [41, 52, 63, 74]
+}
 
-series = pd.Series(data, index=False)
+df = pd.DataFrame(exp, index=[1, 2, 3, 4])
 
-print(series)
+df["job"] = [1234, 3456, 567, 789]
+
+new_row = pd.DataFrame(
+    [
+        {'pizza': 23, 'age': 43, 'job': 543},
+        {'pizza': 296, 'age': 4325, 'job': 554343},
+        {'pizza': 273, 'age': 4343, 'job': 546543}
+    ],
+    index=[5, 6, 7]
+)
+
+df = pd.concat([df, new_row])
+
+print(df)

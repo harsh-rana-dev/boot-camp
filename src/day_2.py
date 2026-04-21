88935aba067fc6570e6c6a45958233053c94c862
harsh_rana_dev <hr2322808@gmail.com>
2026-04-21 21:24:04 +0530

    done

--- src/day_2.py
+++ src/day_2.py
@@ -1,1 +1,23 @@
-import pytest 
+import pytest
+
+@pytest.fixture
+def sample_data():
+    return [
+        {"ticker": "AAPL", "price": 100},
+        {"ticker": "AAPL", "price": 200},
+    ]
+
+def test_fix(sample_data):
+    result = avg_price(sample_data)
+    assert result["AAPL"] == 150
+
+def test_normalize():
+    data = [
+        {"price": 100},
+        {"price": 200},
+    ]
+
+    result = normalize_prices(data)
+
+    assert result[0]["normalized"] == 0.0
+    assert result[1]["normalized"] == 1.0
